4aec9735935b80441bca16eadda5666a183321de
Shashank Bhardwaj <shanksmu@gmail.com>
2024-05-09 01:36:43 +0000

    added html and css

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_hugging_face_embeddings
+from langchain_pinecone import PineconeVectorStore
+import pinecone
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import CTransformers
+from langchain.chains import RetrievalQA
+from accelerate import Accelerator
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
+
+
+embeddings = download_hugging_face_embeddings()
+
+
+index_name="gen-ai-chatbot"
+
+#Loading the index
+docsearch=PineconeVectorStore.from_existing_index(index_name, embeddings)
+
+
+PROMPT=PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+
+chain_type_kwargs={"prompt": PROMPT}
+
+accelerator = Accelerator()
+
+config={
+    'max_new_tokens':512,
+     'temperature':0.8,
+     'gpu_layers':50
+      }
+
+llm=CTransformers(model="/home/sbhardwa/AI_Chatbot/model/llama-2-7b-chat.ggmlv3.q4_0.bin",
+                  model_type="llama",
+                  config=config)
+
+llm, config = accelerator.prepare(llm, config)
+
+
+qa=RetrievalQA.from_chain_type(
+    llm=llm, 
+    chain_type="stuff", 
+    retriever=docsearch.as_retriever(search_kwargs={'k': 2}),
+    return_source_documents=True, 
+    chain_type_kwargs=chain_type_kwargs)
+
+
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    result=qa({"query": input})
+    print("Response : ", result["result"])
+    return str(result["result"])
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port= 8080, debug= True)

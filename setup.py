d89d3a9e4a26f75d687dcccb7cded8a3f88aa37e
Shashank Bhardwaj <shanksmu@gmail.com>
2024-05-09 00:03:04 +0000

    added setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Gen AI Chatbot',
+    version= '1.0.0',
+    author= 'Shashank',
+    author_email= 'shanksmu@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
